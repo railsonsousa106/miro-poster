00ef224fd412657a79092cb079cbc96611c1f303
railsonsousa106 <railsonsousa106@gmail.com>
2020-09-24 16:33:17 -0400

    initial commit

---
+++ js/poster.js
@@ -0,0 +1,36 @@
+const posters = [
+  { src: 'img/posters/1.png', width: 267.02, height: 218.31 }
+]
+
+function getImageHTML(img) {
+  return `
+      <div class="poster">
+        <img src="${img.src}" data-image-url="https://6dd5c9a21ba3.ngrok.io/${img.src}">
+      </div>
+    `;
+}
+
+function addPosters(container) {
+  container.innerHTML += posters.map((poster) => getImageHTML(poster)).join('')
+}
+
+function createPoster(url) {
+  return miro.board.widgets.create({
+    type: 'image',
+    url: url,
+    x: 0,
+    y: 0,
+  })
+}
+
+function bootstrap() {
+  const container = document.getElementById('container');
+  addPosters(container);
+
+  $('.poster img').click(async function () {
+    const widget = (await createPoster($(this).attr('data-image-url')))[0];
+    miro.board.viewport.zoomToObject(widget);
+  });
+}
+
+miro.onReady(bootstrap);
